5e5f4281dbd6d51f1785f9def548441a5909f37c
mhrynko <glukym@gmail.com>
2022-07-20 17:22:28 +0300

    test and solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Pagination } from './components/Pagination';
+import { getNumbers } from './utils';
 
-const App: React.FC = () => <h1>Pagination</h1>;
+const App: React.FC = () => {
+  const [page, setPage] = useState(1);
+  const [perPage, setPerPage] = useState(5);
+  const [items, setItems] = useState<string[]>([]);
+
+  useEffect(() => {
+    setItems(
+      getNumbers(1, 42)
+        .map(n => `Item ${n}`),
+    );
+  }, []);
+
+  const start = (page - 1) * perPage;
+  const end = Math.min(page * perPage, items.length);
+  const visibleItems = items.slice(start, end);
+
+  return (
+    <div className="container">
+      <h1>Items with Pagination</h1>
+
+      <p className="lead" data-cy="info">
+        {`Page ${page} (items ${start + 1} - ${end} of ${items.length})`}
+      </p>
+
+      <div className="form-group row">
+        <div className="col-3 col-sm-2 col-xl-1">
+          <select
+            data-cy="perPageSelector"
+            id="perPageSelector"
+            className="form-control"
+            value={perPage}
+            onChange={event => {
+              setPerPage(+event.target.value);
+              setPage(1);
+            }}
+          >
+            <option value="3">3</option>
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+          </select>
+        </div>
+
+        <label htmlFor="perPageSelector" className="col-form-label col">
+          items per page
+        </label>
+      </div>
+
+      <Pagination
+        total={items.length}
+        currentPage={page}
+        perPage={perPage}
+        onPageChange={setPage}
+      />
+
+      <ul>
+        {visibleItems.map(item => (
+          <li key={item} data-cy="item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,86 @@
+import classNames from 'classnames';
+import React from 'react';
+import { getNumbers } from '../../utils';
+
+type Props = {
+  total: number,
+  perPage: number,
+  currentPage: number,
+  onPageChange: (page: number) => void,
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  currentPage,
+  perPage,
+  onPageChange,
+}) => {
+  const lastPage = Math.ceil(total / perPage);
+  const pages = getNumbers(1, lastPage);
+
+  return (
+    <ul className="pagination">
+      <li
+        className={classNames('page-item', {
+          disabled: currentPage === 1,
+        })}
+      >
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={currentPage === 1}
+          onClick={() => {
+            if (currentPage !== 1) {
+              onPageChange(currentPage - 1);
+            }
+          }}
+        >
+          «
+        </a>
+      </li>
+
+      {pages.map(page => (
+        <li
+          key={page}
+          className={classNames('page-item', {
+            active: page === currentPage,
+          })}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${page}`}
+            onClick={() => {
+              if (page !== currentPage) {
+                onPageChange(page);
+              }
+            }}
+          >
+            {page}
+          </a>
+        </li>
+      ))}
+
+      <li
+        className={classNames('page-item', {
+          disabled: currentPage === lastPage,
+        })}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={currentPage === lastPage}
+          onClick={() => {
+            if (currentPage !== lastPage) {
+              onPageChange(currentPage + 1);
+            }
+          }}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
